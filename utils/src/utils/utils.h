189b4c39cbb3701c553d16a98c14b3158e4bad6b
Facundo Heras <fheras@frba.utn.edu.ar>
2024-04-15 01:05:25 +0000

    Cambios en la memoria y utils

---
+++ utils/src/utils/utils.h
@@ -0,0 +1,65 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<commons/log.h>
+#include<commons/collections/list.h>
+#include<commons/config.h>
+#include<string.h>
+#include<assert.h>
+#include<signal.h>
+
+
+
+#define PUERTO "4444"
+
+typedef enum
+{
+	MENSAJE,
+	PAQUETE
+}op_code;
+
+typedef struct
+{
+	int size;
+	void* stream;
+} t_buffer;
+
+typedef struct
+{
+	op_code codigo_operacion;
+	t_buffer* buffer;
+} t_paquete;
+
+
+
+
+extern t_log* logger;
+
+
+
+
+extern t_log* logger;
+
+void* recibir_buffer(int*, int);
+
+int iniciar_servidor(void);
+int esperar_cliente(int);
+t_list* recibir_paquete(int);
+void recibir_mensaje(int);
+int recibir_operacion(int);
+t_paquete* crear_paquete(void);
+void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio);
+void enviar_paquete(t_paquete* paquete, int socket_cliente);
+void liberar_conexion(int socket_cliente);
+void eliminar_paquete(t_paquete* paquete);
+void iterator(char* value);
+void terminar_programa(int conexion, t_log* logger, t_config* config);
+t_config* iniciar_config(void);
+
+
+#endif /* UTILS_H_ */
