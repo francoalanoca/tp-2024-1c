54e197741dd59ad0e9ed9c158b86fecdedf9129c
Aganem <abdulganem@gmail.com>
2024-06-25 17:44:52 +0000

    cambio server

--- memoria/src/conexion.c
+++ memoria/src/conexion.c
@@ -6,48 +6,63 @@ int iniciar_servidor_memoria()
 {
 
 	int memoria_server_fd, socket_cliente;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+    int err;
+    char ip_str[INET_ADDRSTRLEN];
+    struct addrinfo hints, *server_info;
+    char puerto[6];
+  struct addrinfo *p ;
+    sprintf(puerto, "%d", cfg_memoria->PUERTO_ESCUCHA);
+    
+    memset(&hints, 0, sizeof(hints)); 
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-    // Creating socket file descriptor
-    if ((memoria_server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
+
+    err = getaddrinfo(NULL, puerto, &hints, &server_info);
+   
+      for (p= server_info; p != NULL; p = p->ai_next) {
+        if ((memoria_server_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("Falló el socket");
+            continue;
+        }
+
+        if (bind(memoria_server_fd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(memoria_server_fd);
+            perror("Falló el bind");
+            continue;
+        }
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "Falló el bind con todas las direcciones\n");
         exit(EXIT_FAILURE);
     }
+
+    inet_ntop(server_info->ai_family, server_info->ai_addr, ip_str, sizeof(ip_str));
+    freeaddrinfo(server_info);
+   
 
    
-    if (setsockopt(memoria_server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    address.sin_port = htons(cfg_memoria->PUERTO_ESCUCHA); 
-
-    // Binding el socket al puerto dado 
-    if (bind(memoria_server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
     
-    if (listen(memoria_server_fd, 3) < 0) {
-        perror("listen");
+    if (listen(memoria_server_fd, SOMAXCONN) < 0) {
+        perror("Falló el listen");
         exit(EXIT_FAILURE);
-    }
-    char ip_str[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(address.sin_addr), ip_str, INET_ADDRSTRLEN);
-    log_info(logger_memoria, "Servidor MEMORIA está escuchando en %s:%d...\n", ip_str, ntohs(address.sin_port));
+    }    
+  
+   
+    log_info(logger_memoria, "Servidor MEMORIA está escuchando en %s:%d...\n", ip_str, cfg_memoria->PUERTO_ESCUCHA);
 
 
 while (1) {
         // Espera al cliente
-        if ((socket_cliente = accept(memoria_server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-            perror("accept");
+        if ((socket_cliente = accept(memoria_server_fd, NULL,NULL)) < 0) {
+            perror("Falló aceptar");
             exit(EXIT_FAILURE);
         }
 
-        printf("Servidor MEMORIA recibió conexión cliente en %s:%d\n", ip_str, ntohs(address.sin_port));
+        printf("Servidor MEMORIA recibió conexión cliente en %s:%d...\n", ip_str, cfg_memoria->PUERTO_ESCUCHA);
 
         pthread_t client_thread;
         int* socket_cliente_hilo = malloc(sizeof(int));
